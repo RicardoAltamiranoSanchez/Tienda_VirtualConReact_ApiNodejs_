ef4fd5be920a0a0ba6381db169d410fdc6e45744
Ricardo <altamiranoricardo545@gmail.com>
2022-01-29 23:27:30 -0600

    r

--- src/Paginas/Conocenos.js
+++ src/Paginas/Conocenos.js
@@ -32,30 +32,7 @@ return (
             
           
           </p>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Gravida
-            neque convallis a cras. Id nibh tortor id aliquet lectus proin nibh
-            nisl. Pharetra sit amet aliquam id. Sollicitudin ac orci phasellus
-            egestas tellus rutrum tellus. Rhoncus est pellentesque elit
-            ullamcorper dignissim cras. Cras pulvinar mattis nunc sed blandit
-            libero volutpat sed cras. Ultrices in iaculis nunc sed augue lacus
-            viverra vitae. Id diam maecenas ultricies mi. Euismod quis viverra
-            nibh cras pulvinar mattis nunc. Phasellus vestibulum lorem sed risus
-            ultricies tristique. Ut placerat orci nulla pellentesque dignissim.
-            Praesent tristique magna sit amet. Mattis aliquam faucibus purus in.
-            Vitae elementum curabitur vitae nunc sed velit dignissim sodales.
-            Neque egestas congue quisque egestas diam in arcu. Ultrices eros in
-            cursus turpis massa tincidunt dui ut. Mi proin sed libero enim.
-            Semper viverra nam libero justo laoreet sit. Maecenas volutpat
-            blandit aliquam etiam erat. In nisl nisi scelerisque eu. Tortor at
-            risus viverra adipiscing at in tellus integer feugiat. Integer
-            malesuada nunc vel risus commodo. Sit amet tellus cras adipiscing
-            enim eu. Faucibus turpis in eu mi bibendum neque egestas congue
-            quisque. Ligula ullamcorper malesuada proin libero. Adipiscing diam
-            donec adipiscing tristique risus nec feugiat in. Sed lectus
-          
-          </p>
+       
         </div>
         <img src="../../img/nosotros.jpg" alt="Aqui va la imagen de nosostros"/>
       </div>
